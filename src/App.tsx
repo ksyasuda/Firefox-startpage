2121916b3531a515cab15faf2d44b4f999ca03ae
ksyasuda <ksyasuda@umich.edu>
2020-10-25 16:03:22 -0400

    Fix style on desktop and ipad and made it so MLB component doesn't do a million requests

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,33 @@ import NcaaFootball from "./containers/NcaaFootball/NcaaFootball"
 import "./App.css"
 
 function App() {
-	return (
-		<div className={classes.Container}>
-			<Helmet>
-				<style>{"body { background-color: #263238}"}</style>
-				<title>{"Sudacode Startpage"}</title>
-			</Helmet>
-			<img src={Fox} alt='Firefox' className={classes.Fox} />
-			<h1 style={{ color: "#6b2144" }}>Welcome Freud</h1>
-			<Gsearch />
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					marginTop: "2%",
-					position: "relative",
-				}}
-			>
-				<Favorites />
-				<Docs />
-				<Media />
-				<MoreLinks />
-			</div>
-			<Weather />
-			<Mlb />
-			<NcaaFootball />
-		</div>
-	)
+    return (
+        <div className={classes.Container}>
+            <Helmet>
+                <style>{"body { background-color: #263238}"}</style>
+                <title>{"Sudacode Startpage"}</title>
+            </Helmet>
+            <img src={Fox} alt='Firefox' className={classes.Fox} />
+            <h1 style={{ color: "#6b2144" }}>Welcome Freud</h1>
+            <Gsearch />
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "2%",
+                    position: "relative",
+                }}
+            >
+                <Favorites />
+                <Docs />
+                <Media />
+                <MoreLinks />
+            </div>
+            <Weather />
+            <Mlb />
+            <NcaaFootball />
+        </div>
+    )
 }
 
 export default App
--- src/containers/Mlb/Mlb.tsx
+++ src/containers/Mlb/Mlb.tsx
@@ -2,129 +2,121 @@ import React, { useState, useEffect } from "react"
 import classes from "./Mlb.module.css"
 import axios from "axios"
 
-const Mlb: React.FC<{}> = () => {
-	const colors = useState({
-		TB: { abbrev: "TB", color: "#092C5C" },
-		LAD: { abbrev: "LAD", color: "#0057a5" },
-	})
-	const [mlb, setMlb] = useState({
-		score: -1,
-		oppScore: -1,
-		inning: -1,
-		theTeam: "",
-		oppTeam: "",
-		theTeamAbv: "",
-		oppTeamAbv: "",
-		status: "",
-		detail: "",
-		color1: "",
-		color2: "",
-	})
+const Mlb = () => {
+    const colors = useState({
+        TB: { abbrev: "TB", color: "#092C5C" },
+        LAD: { abbrev: "LAD", color: "#0057a5" },
+    })
+    const [hasRun, setHasRun] = useState(false)
+    const [mlb, setMlb] = useState({
+        score: -1,
+        oppScore: -1,
+        inning: -1,
+        theTeam: "",
+        oppTeam: "",
+        theTeamAbv: "",
+        oppTeamAbv: "",
+        status: "",
+        detail: "",
+    })
 
-	const color1 = colors[0].LAD.color
-	const color2 = colors[0].TB.color
+    useEffect(() => {
+        // Don't know why I need this but now it doesn't send a million requests
+        if (!hasRun) {
+            axios
+                .get(
+                    "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams/lad",
+                )
+                .then(data => {
+                    const { nextEvent } = data.data.team
+                    for (let event of nextEvent) {
+                        const eventid = event.id
+                        // get game data
+                        axios
+                            .get(
+                                `https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard/${eventid}`,
+                            )
+                            .then(res => {
+                                //console.log(res)
+                                const competitors = [
+                                    ...res.data.competitions[0].competitors,
+                                ]
+                                const {
+                                    description,
+                                    detail,
+                                } = res.data.competitions[0].status.type
+                                let tscore, oscore, team, oteam, abv, oabv
+                                for (let comp of competitors) {
+                                    //console.log(comp)
+                                    const {
+                                        displayName,
+                                        abbreviation,
+                                    } = comp.team
+                                    const { score } = comp
+                                    if (abbreviation === "LAD") {
+                                        tscore = score
+                                        team = displayName
+                                        abv = abbreviation
+                                    } else {
+                                        oscore = score
+                                        oteam = displayName
+                                        oabv = abbreviation
+                                    }
+                                }
+                                setMlb({
+                                    score: tscore,
+                                    oppScore: oscore,
+                                    inning: -1,
+                                    theTeam: team,
+                                    oppTeam: oteam,
+                                    theTeamAbv: abv,
+                                    oppTeamAbv: oabv,
+                                    status: description,
+                                    detail: detail,
+                                })
+                                setHasRun(true)
+                            })
+                            .catch(err => console.error(err.message))
+                    }
+                })
+                .catch(err => console.error(err.message))
+        }
+    }, [hasRun])
 
+    const style1 = {
+        color: colors[0].LAD.color,
+        backgroundColor: "white",
+        marginRight: "3px",
+    }
 
-	useEffect(() => {
-		axios
-			.get(
-				"https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams/lad"
-			)
-			.then(data => {
-				const { nextEvent } = data.data.team
-				for (let event of nextEvent) {
-					const eventid = event.id
-					// get game data
-					axios
-						.get(
-							`https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard/${eventid}`
-						)
-						.then(res => {
-							//console.log(res)
-							const competitors = [
-								...res.data.competitions[0].competitors,
-							]
-							const {
-								description,
-								detail,
-							} = res.data.competitions[0].status.type
-							let tscore, oscore, team, oteam, abv, oabv
-							for (let comp of competitors) {
-								//console.log(comp)
-								const { displayName, abbreviation } = comp.team
-								const { score } = comp
-								if (abbreviation === "LAD") {
-									tscore = score
-									team = displayName
-									abv = abbreviation
-								} else {
-									oscore = score
-									oteam = displayName
-									oabv = abbreviation
-								}
-							}
-							let color1: string = ""
-							let color2: string = ""
-							switch (oabv) {
-								case "TB":
-									color2 = colors[0].TB.color
-									break
-							}
-							color1 = colors[0].LAD.color
-							setMlb({
-								score: tscore,
-								oppScore: oscore,
-								inning: -1,
-								theTeam: team,
-								oppTeam: oteam,
-								theTeamAbv: abv,
-								oppTeamAbv: oabv,
-								status: description,
-								detail: detail,
-								color1: color1,
-								color2: color2,
-							})
-						})
-						.catch(err => console.error(err.message))
-				}
-			})
-			.catch(err => console.error(err.message))
-	}, [color1, color2, colors])
+    const style2 = {
+        color: colors[0].TB.color,
+        backgroundColor: "#9c6263",
+    }
 
-	const style1 = {
-		color: mlb.color1,
-		backgroundColor: "white",
-		marginRight: "3px",
-	}
-
-	const style2 = {
-		color: mlb.color2,
-		backgroundColor: "#9c6263",
-	}
-
-	return (
-		<div className={classes.BigContainer}>
-			<h2 className={classes.Title}>MLB</h2>
-			<div className={classes.Container}>
-				<div className={classes.Teams}>
-					<span className={classes.Dodgers} style={style1}>
-						{mlb.theTeam}
-					</span>
-					<span className={classes.Divider}>vs.</span>
-					<span className={classes.Other} style={style2}>
-						{mlb.oppTeam}
-					</span>
-				</div>
-				<p className={classes.Scores}>
-					<span className={classes.DodgerScore}>{mlb.score}</span> -
+    return (
+        <div className={classes.BigContainer}>
+            <h2 className={classes.Title}>MLB</h2>
+            <div className={classes.Container}>
+                <div className={classes.Teams}>
+                    <span className={classes.Dodgers} style={style1}>
+                        {mlb.theTeam}
+                    </span>
+                    <span className={classes.Divider}>vs.</span>
+                    <span className={classes.Other} style={style2}>
+                        {mlb.oppTeam}
+                    </span>
+                </div>
+                <p className={classes.Scores}>
+                    <span className={classes.DodgerScore}>{mlb.score}</span> -
 					{"  "}
-					<span className={classes.OtherScore}>{mlb.oppScore}</span>
-				</p>
-				<p className={classes.Status}>{mlb.status}</p>
-				<p className={classes.Detail}>{mlb.detail}</p>
-			</div>
-		</div>
-	)
+                    <span className={classes.OtherScore}>{mlb.oppScore}</span>
+                </p>
+                <p className={classes.Status}>{mlb.status}</p>
+                <p className={classes.Detail}>{mlb.detail}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Mlb
