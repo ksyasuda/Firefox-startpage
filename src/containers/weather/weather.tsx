fbf9ff821fac8ee0e9e3420a9ab8ea58275bd580
ksyasuda <ksyasuda@umich.edu>
2021-05-03 20:56:50 -0700

    update location to los angeles

--- src/containers/weather/weather.tsx
+++ src/containers/weather/weather.tsx
@@ -1,53 +1,72 @@
-import React, { useState, useEffect } from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThermometerFull, faSun, faTemperatureHigh, faTemperatureLow, faCloudSunRain, faWind } from '@fortawesome/free-solid-svg-icons'
-import classes from "./weather.module.css"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faThermometerFull,
+	faSun,
+	faTemperatureHigh,
+	faTemperatureLow,
+	faCloudSunRain,
+	faWind,
+} from "@fortawesome/free-solid-svg-icons";
+import classes from "./weather.module.css";
+import axios from "axios";
 
 const Weather: React.FC<{}> = () => {
-	const [currentWeather, setCurrentWeather] = useState(null)
-	const [feelsLike, setFeelsLike] = useState(null)
-	const [highTemp, setHighTemp] = useState(null)
-	const [lowTemp, setLowTemp] = useState(null)
-	const [description, setDescription] = useState(null)
-	const [wind, setWind] = useState(null)
+	const [currentWeather, setCurrentWeather] = useState(null);
+	const [feelsLike, setFeelsLike] = useState(null);
+	const [highTemp, setHighTemp] = useState(null);
+	const [lowTemp, setLowTemp] = useState(null);
+	const [description, setDescription] = useState(null);
+	const [wind, setWind] = useState(null);
 	useEffect(() => {
-		const LOCATION = 'Ann+Arbor'
-		// const LOCATION = 'Los+Angeles'
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${LOCATION}&units=imperial&appid=${process.env.REACT_APP_WEATHER_KEY}`
-		const prom1 = axios.get(url)
+		// const LOCATION = 'Ann+Arbor'
+		const LOCATION = "Los+Angeles";
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${LOCATION}&units=imperial&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+		const prom1 = axios.get(url);
 		//`https://api.openweathermap.org/data/2.5/weather?q=Ann+Arbor&units=imperial&appid=${process.env.REACT_APP_WEATHER_KEY}`
 		prom1.then((res: any) => {
 			// console.log(res)
-			const { main, weather, wind } = res.data
-			setCurrentWeather(main.temp)
-			setFeelsLike(main.feels_like)
-			setHighTemp(main.temp_max)
-			setLowTemp(main.temp_min)
-			setDescription(weather[0].description)
-			setWind(wind.speed)
-		})
-		prom1.catch(err => console.error(err.message))
-	}, [])
+			const { main, weather, wind } = res.data;
+			setCurrentWeather(main.temp);
+			setFeelsLike(main.feels_like);
+			setHighTemp(main.temp_max);
+			setLowTemp(main.temp_min);
+			setDescription(weather[0].description);
+			setWind(wind.speed);
+		});
+		prom1.catch((err) => console.error(err.message));
+	}, []);
 	return (
 		<>
 			<h2 className={classes.Title}>Weather </h2>
 			<div className={classes.WeatherContainer}>
-				<span className={classes.WeatherInfo}><FontAwesomeIcon icon={faThermometerFull}/> {currentWeather}℉</span>
 				<span className={classes.WeatherInfo}>
-					<span className={classes.Icon}><FontAwesomeIcon icon={faSun}/></span> {feelsLike}℉
+					<FontAwesomeIcon icon={faThermometerFull} />{" "}
+					{currentWeather}℉
+				</span>
+				<span className={classes.WeatherInfo}>
+					<span className={classes.Icon}>
+						<FontAwesomeIcon icon={faSun} />
+					</span>{" "}
+					{feelsLike}℉
 				</span>
 				<p className={classes.WeatherInfo} id={classes.HighLow}>
-					<FontAwesomeIcon icon={faTemperatureHigh}/> {highTemp}℉  <span id={classes.divider}>|</span> <FontAwesomeIcon icon={faTemperatureLow}/> {lowTemp}℉
+					<FontAwesomeIcon icon={faTemperatureHigh} /> {highTemp}℉{" "}
+					<span id={classes.divider}>|</span>{" "}
+					<FontAwesomeIcon icon={faTemperatureLow} /> {lowTemp}℉
 				</p>
 				<span className={classes.WeatherInfo}>
-					<span className={classes.Icon}><FontAwesomeIcon icon={faCloudSunRain}/></span>
+					<span className={classes.Icon}>
+						<FontAwesomeIcon icon={faCloudSunRain} />
+					</span>
 					{description}
 				</span>
-				<span className={classes.WeatherInfo}><FontAwesomeIcon icon={faWind}/> {wind} MPH</span>
+				<span className={classes.WeatherInfo}>
+					<FontAwesomeIcon icon={faWind} /> {wind} MPH
+				</span>
 			</div>
 		</>
-	)
-}
+	);
+};
 
-export default Weather
+export default Weather;
